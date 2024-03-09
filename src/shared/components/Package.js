5a3d174bc65ee7ab08b60861581a220aed4647ba
giankoenig <gian.koenig@protonmail.com>
2024-03-09 16:03:48 +0100

    latest change

---
+++ src/shared/components/Package.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Package = () => {
+    return ();
+};
+
+export default Package;
---
+++ src/shared/components/SessionList.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+const SessionList = () => {};
+
+export default SessionList;
